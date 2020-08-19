0a9e8c338943972c24a1aee89bd82fbfbca3ade9
Harry Peirse <peirse@gmail.com>
2020-08-19 20:02:03 +0100

    Tileset Cleanup

--- tilesets/GeneralTiles.gd
+++ tilesets/GeneralTiles.gd
@@ -1,17 +1,10 @@
 extends TileMap
 
-export var cancelBulletCollider : PackedScene
-export var cancelCrystalCollider : PackedScene
+export var spikeCollider : PackedScene
 
 func _ready():
-	var cancelBulletId = tile_set.find_tile_by_name("CancelBullet")
-	for vector in get_used_cells_by_id(cancelBulletId):
-		var collider: Area2D = cancelBulletCollider.instance()
-		collider.global_position = map_to_world(vector)
-		add_child(collider)
-	
-	var cancelCrystalId = tile_set.find_tile_by_name("CancelCrystal")
-	for vector in get_used_cells_by_id(cancelCrystalId):
-		var collider: Area2D = cancelCrystalCollider.instance()
+	var id = tile_set.find_tile_by_name("Spikes")
+	for vector in get_used_cells_by_id(id):
+		var collider: Area2D = spikeCollider.instance()
 		collider.global_position = map_to_world(vector)
 		add_child(collider)
--- tilesets/SpecialTiles.gd
+++ tilesets/SpecialTiles.gd
@@ -1,4 +1,17 @@
 extends TileMap
 
+export var cancelBulletCollider : PackedScene
+export var cancelCrystalCollider : PackedScene
+
 func _ready():
-	pass # Replace with function body.
+	var cancelBulletId = tile_set.find_tile_by_name("CancelBullet")
+	for vector in get_used_cells_by_id(cancelBulletId):
+		var collider: Area2D = cancelBulletCollider.instance()
+		collider.global_position = map_to_world(vector)
+		add_child(collider)
+	
+	var cancelCrystalId = tile_set.find_tile_by_name("CancelCrystal")
+	for vector in get_used_cells_by_id(cancelCrystalId):
+		var collider: Area2D = cancelCrystalCollider.instance()
+		collider.global_position = map_to_world(vector)
+		add_child(collider)
